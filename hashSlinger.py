c9a51bbd7f8cce1f20f349ea0f50b022f8d934e7
Astrixo <63420382+Astrixo@users.noreply.github.com>
2025-09-03 12:52:17 -0400

    added things idfk

--- hashSlinger.py
+++ hashSlinger.py
@@ -12,7 +12,7 @@ GREENTEXT = "\033[32m" #Success
 YELLOWTEXT = "\033[33m" #Errors :(
 BLUETEXT = "\033[34m" #I just like this color
 RETURNDEFAULTCOLOR = "\033[0m" #Default term color
-RANDOMHASHES = ['sha512', 'sha256', 'sha224', 'sha384']
+ROCKYOUHASHES = ['sha512', 'sha256', 'sha224', 'sha384']
 BARRIER = "#########################################"
 
 def pick_randomLine():
@@ -20,9 +20,8 @@ def pick_randomLine():
         lines = passwordList.readlines()
         return random.choice(lines).strip()
 
-
 def level_one():
-    intro("LEVEL ONE")
+    block("LEVEL ONE")
     print('General: You will be given an MD5 Hash of a password randomly picked from the RockYou wordlist.')
     print('Instructions: Enter the plaintext password associated with this MD5 Hash.')
     print('Hint: hashcat -a 0 -m 0 <hash.txt> /usr/share/wordlists/rockyou.txt')
@@ -33,7 +32,7 @@ def level_one():
     guess(password)
 
 def level_two():
-    intro("LEVEL TWO")
+    block("LEVEL TWO")
     print('General: Good job on solving the MD5 Hash. This one is SHA256.')
     print('Instructions: Enter the plaintext password associated with this SHA256 Hash.')
     print('Hint: It will not be -m 0. It will be -m 1400')
@@ -44,7 +43,7 @@ def level_two():
     guess(password)
 
 def level_three():
-    intro("LEVEL THREE")
+    block("LEVEL THREE")
     print('General: Nice! Now im going to give you a hash without telling you the algorithm')
     print('Instructions: Enter the plaintext password associated with this unkown Hash.')
     print('Hint: Use hashes.com hash identifier or run hashcat on the file with no arguments')
@@ -55,18 +54,18 @@ def level_three():
     guess(password)
 
 def level_four():
-    intro("LEVEL FOUR")
+    block("LEVEL FOUR")
     print('General: Now that you have figured out how to identify hashes. Im going to give you a random one.')
     print('Instructions: Enter the plaintext password associated with this random Hash.')
     print('Picking a password...')
     password = pick_randomLine()
     print('Password picked!')
-    hashType = random.choice(RANDOMHASHES)
+    hashType = random.choice(ROCKYOUHASHES)
     print(f'Target Hash: {hash(hashType, password)}')
     guess(password)
 
 def level_five():
-    intro("LEVEL FIVE")
+    block("LEVEL FIVE")
     print("General: Now we're moving onto something new! Mask attacks.")
     print("More General: NVM")
 
@@ -84,25 +83,42 @@ def guess(password):
         guess = input("Guess: ")
     print(f'{GREENTEXT}Correct!{RETURNDEFAULTCOLOR}')
 
-def intro(title):
+def block(message):
     print(BARRIER)
-    print(f'##{BLUETEXT}{title.center(len(BARRIER)-4)}{RETURNDEFAULTCOLOR}##')
+    print(f'##{BLUETEXT}{message.center(len(BARRIER)-4)}{RETURNDEFAULTCOLOR}##')
     print(BARRIER)
 
-def win():
-    print("Congratulation! This concludes the levels currently available!")
-    print("Play again for even more training.")
-
-def main():
-    print(BARRIER)
-    print(f'##{BLUETEXT}Preparing the Hash Slinger Training{RETURNDEFAULTCOLOR}##')
+def dictionary_attacks():
     print(f'##{BLUETEXT}{"Dictionary Attacks".center(len(BARRIER)-4)}{RETURNDEFAULTCOLOR}##')
     level_one()
     level_two()
     level_three()
     level_four()
-    print(f'##{BLUETEXT}{"Dictionary Attacks".center(len(BARRIER)-4)}{RETURNDEFAULTCOLOR}##')
-    #level_five()
+    level_five()
+
+def pick_module():
+    print("What module would you like to work on?")
+    answers = ["1", "2", "3"]
+    print("  [1] Dictionary Attacks\n  [2] Mask Attacks\n  [3] Combinator attacks")
+    answer = str(input("Guess: "))
+    while answer not in answers:
+        print(f"{REDTEXT}THAT IS NOT A VALID ANSWER >:[{RETURNDEFAULTCOLOR}")
+        answer = str(input("Guess: "))
+    if answer is "1":
+        dictionary_attacks()
+    elif answer is "2":
+        print("This functionality is not added yet rip")
+    else:
+        print("This functionality is not added yet rip")
+
+def win():
+    print("Congratulation! This concludes the levels currently available!")
+    print("Play again for even more training.")
+
+
+def main():
+    block("Preparing the Hash Slinger Training")
+    pick_module()
     win()
 
 
